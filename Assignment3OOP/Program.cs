8ab9ada5c882d72fe8bf3fb7359645960e244d82
Marwa Mahmoud <mm3764453@gmail.com>
2024-07-15 20:05:20 +0300

    Add project files.

---
+++ Assignment3OOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment3OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
