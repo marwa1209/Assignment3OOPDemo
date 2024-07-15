b239c66ff3f4eb2a49073ae078ba709cae2e34db
Marwa Mahmoud <mm3764453@gmail.com>
2024-07-15 20:22:08 +0300

    Ploymorphism[Overloading]

--- Assignment3OOP/Program.cs
+++ Assignment3OOP/Program.cs
@@ -2,9 +2,38 @@
 {
     internal class Program
     {
+        #region overloading
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Sum(int x, int y,int z)
+        {
+            return x + y + z;
+        }
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Sum(int x, double y)
+        {
+            return x + y;
+        }
+        public static double Sum(double x,  int y)
+        {
+            return x + y;
+        }
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Ploymorphism[Overloading]
+            int result = Sum(1, 2);
+            Console.WriteLine(result);
+            int result2 = Sum(1, 2,3);
+            Console.WriteLine(result2);
+            double result3 = Sum(1.2, 2.2);
+            Console.WriteLine(result3);
+            #endregion
         }
     }
 }
